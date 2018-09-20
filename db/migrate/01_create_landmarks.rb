e515f43fee7f8a73452200cdc7cc4378d571998e
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-20 04:39:31 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,40 @@
 class CreateLandmarks < ActiveRecord::Migration
   raise 'Write CreateLandmarks migration here'
 end
+
+  def change
+    create_table :landmarks do |t|
+      t.string :id
+      t.string :name
+      t.string :figure_id
+      t.string :year_completed
+  end
+
+  def change
+    create_table :figures do |t|
+      t.string :id
+      t.string :name
+  end
+
+  def change
+    create_table :titles do |t|
+      t.string :id
+      t.string :name
+  end
+
+  def change
+    create_table :landmarks do |t|
+      t.string :id
+      t.string :name
+      t.string :figure_id
+      t.string :year_completed
+  end
+end
+
+class Landmark < ApplicationRecord
+  belongs_to :figures
+end
+
+class Figure < ApplicationRecord
+  belongs_to :landmarks
+end
