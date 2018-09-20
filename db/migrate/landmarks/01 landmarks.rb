f9ccaba8df9a696282b573eb83674d5e6a00404f
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-20 05:17:54 +0000

    automatically backed up by learn

--- db/migrate/landmarks/01 landmarks.rb
+++ db/migrate/landmarks/01 landmarks.rb
@@ -1,10 +1,10 @@
-class CreateLandmarks < ActiveRecord::Migration
-  def change
-    create_table :landmarks do |t|
-      t.string :id
-      t.string :name
-      t.string :figure_id
-      t.string :year_completed
-  end
-end
-end
+# class CreateLandmarks < ActiveRecord::Migration
+#   def change
+#     create_table :landmarks do |t|
+#       t.string :id
+#       t.string :name
+#       t.string :figure_id
+#       t.string :year_completed
+#   end
+# end
+# end
