4506977e434978617f23b2d4d48b7b82277a7de0
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-09-20 05:13:30 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -2,51 +2,5 @@ class CreateLandmarks < ActiveRecord::Migration
   raise 'Write CreateLandmarks migration here'
 end
 
-  def change
-    create_table :landmarks do |t|
-      t.string :id
-      t.string :name
-      t.string :figure_id
-      t.string :year_completed
-  end
 
-  def change
-    create_table :figures do |t|
-      t.string :id
-      t.string :name
-  end
 
-  def change
-    create_table :titles do |t|
-      t.string :id
-      t.string :name
-  end
-
-  def change
-    create_table :landmarks do |t|
-      t.string :id
-      t.string :name
-      t.string :figure_id
-      t.string :year_completed
-  end
-end
-
-class Landmark < ApplicationRecord
-  belongs_to :figures
-end
-
-class Figure < ApplicationRecord
-  has_many :landmarks
-  has_many :figure_titles
-  has_many :titles, through :figure_titles
-end
-
-class FigureTitle < ApplicationRecord
-  belongs_to :figures
-  has_one :title
-end
-
-class Title < ApplicationRecord
-  has_many :figure_titles
-  has_many :titles, through :figure_titles
-end
